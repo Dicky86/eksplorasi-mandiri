af447fc884c8515fbdeef85b0193f76d9951b34e
Moh. Dicky Purnama <114645106+Dicky86@users.noreply.github.com>
2024-06-19 15:00:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$string = 'Ini adalah tipe data string';
+$int    = 99;
+$float  = 3.7;
+
+echo 'Ini adalah output dari print_r()';
+echo '<br/>';
+print_r($string);
+echo '<br/>';
+print_r($int);
+echo '<br/>';
+print_r($float);
+echo '<br/>';
+echo '<br/>';
+
+
+
+echo 'Ini adalah output dari var_dump()';
+echo '<br/>';
+var_dump($string);
+echo '<br/>';
+var_dump($int);
+echo '<br/>';
+var_dump($float);
+echo '<br/>';
